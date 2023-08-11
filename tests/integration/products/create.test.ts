27b50fc7507ee7d2967651c9182bfe5a76c98b79
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-11 15:02:40 -0300

    test(integration): test of integration on POST /product

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-
+import mock from '../../mocks/products.mock'
+import app from '../../../src/app';
+import Product from '../../../src/database/models/product.model';
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
-
+  it.skip('should return 201 when product is created', async function () {
+    //arrange
+    const httpRequest = mock.newProduct
+    const mockResponse = Product.build(mock.productCreated)
+    sinon.stub(Product, 'create').resolves(mockResponse)
+    //act
+    const httpResponse = await chai.request(app).post('/products').send(httpRequest)  
+    //assert
+    expect(httpResponse.status).to.equal(mock.mapStatusCode.CREATED)
+    expect(httpResponse.body).to.be.deep.equal(mockResponse)
+  })
 });
---
+++ tests/mocks/products.mock.ts
@@ -0,0 +1,22 @@
+ const productCreated = {
+  id: 6,
+  name: "Martelo de Thor",
+  price: "30 peças de ouro"
+}
+
+ const newProduct = {
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  orderId: 4
+}
+
+ const mapStatusCode = {
+  "OK": 200,
+  "CREATED": 201,
+}
+
+export default { 
+  productCreated,
+  newProduct,
+  mapStatusCode
+}

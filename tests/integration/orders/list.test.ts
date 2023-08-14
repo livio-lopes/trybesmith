755a772a4a6e25071b08b3e346dc00e53d62b1c5
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-14 16:20:33 -0300

    refactor(test): refactor test integration of endpoint GET /orders

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -2,21 +2,20 @@ import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import ordersMock from '../../mocks/orders.mock';
-import OrderModel from '../../../src/database/models/order.model';
 import app from '../../../src/app';
+const OrderModel = require('../../../src/database/models/order.model').default;
 
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
   it('should return 200 when orders are listed', async function () {
-    //arrange
-    const mockResponse = ordersMock.listed.map(order => OrderModel.build(order))
-    sinon.stub(OrderModel, 'findAll').resolves(mockResponse)
-    //act
-    const httpResponse = await chai.request(app).get('/orders')
-    //assert
-    expect(httpResponse.status).to.equal(ordersMock.statusCode.OK)
-    expect(httpResponse.body).to.be.deep.equal(ordersMock.listed)
+   //arrange
+    sinon.stub(OrderModel, 'findAll').resolves(ordersMock.listOrders as any)
+   //act
+    const response = await chai.request(app).get('/orders')
+   //assert
+   expect(response.status).to.be.equal(ordersMock.statusCode.OK)
+   expect(response.body).to.be.deep.equal(ordersMock.responseService)
   })
 });
--- tests/mocks/orders.mock.ts
+++ tests/mocks/orders.mock.ts
@@ -1,16 +1,51 @@
-const listed = [
+const listOrders = [
+  {dataValues: 
   {
     "id": 1,
-    "userId": 2,
-    "productIds": [1, 2]
+    "userId": 1,
+    "productIds": [
+      {
+        "id": 1
+      },
+      {
+        "id": 2
+      }
+    ]
+  }},
+  {dataValues:
+  {
+    "id": 2,
+    "userId": 3,
+    "productIds": [
+      {
+        "id": 3
+      },
+      {
+        "id": 4
+      }
+    ]
+  }},
+]
+
+const responseService = [
+  {
+    "id": 1,
+    "userId": 1,
+    "productIds": [
+      1,
+      2
+    ]
   },
   {
     "id": 2,
-    "userId": 1,
-    "productIds": [3, 4]
-  }
+    "userId": 3,
+    "productIds": [
+      3,
+      4
+    ]
+  },
 ]
 
 const statusCode = { OK:200}
 
-export default { listed, statusCode }
+export default { listOrders, statusCode,responseService }

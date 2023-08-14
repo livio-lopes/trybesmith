0dc9300ae0d842b8bc6186ec8a8eefae1b5354d1
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-14 16:53:04 -0300

    refactor(products): refactor types returns of products on controller layer

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,15 +2,15 @@ import { Request, Response } from 'express';
 import productsService from '../services/products.service';
 import statusCode from '../utlis/statusCode';
 
-const create = async (req: Request, res: Response): Promise<void> => {
+const create = async (req: Request, res: Response): Promise<Response> => {
   const { name, price, orderId } = req.body;
   const newProduct = await productsService.create({ name, price, orderId });
-  res.status(statusCode.CREATED).json(newProduct);
+  return res.status(statusCode.CREATED).json(newProduct);
 };
 
-const listProducts = async (req: Request, res: Response): Promise<void> => {
+const listProducts = async (req: Request, res: Response): Promise<Response> => {
   const products = await productsService.listProducts();
-  res.status(statusCode.OK).json(products);
+  return res.status(statusCode.OK).json(products);
 };
 
 export default { create, listProducts };

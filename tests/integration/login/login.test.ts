e1911227c63714163389b007aefadfb14249bfcd
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-16 11:07:12 -0300

    test(integration): test integration on endpoint POST /login

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -1,10 +1,62 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import loginMock from '../../mocks/login.mock';
+import app from '../../../src/app';
+import UserModel from '../../../src/database/models/user.model';
 
 chai.use(chaiHttp);
 
 describe('POST /login', function () { 
   beforeEach(function () { sinon.restore(); });
-
+  it('should return 400 when username is not provided', async function () {
+    //arrange
+    const httpBody = loginMock.noUserLogin
+    //act
+    const httpResponse = await chai.request(app).post('/login').send(httpBody)
+    //assert
+    expect(httpResponse.status).to.equal(loginMock.status.BAD_REQUEST)
+    expect(httpResponse.body).to.be.deep.equal(loginMock.errorMessage.REQUIRED_FIELD)
+  })
+  it('should return 400 when password is not provided', async function () {
+    //arrange
+    const httpBody = loginMock.noPasswordLogin
+    //act
+    const httpResponse = await chai.request(app).post('/login').send(httpBody)
+    //assert
+    expect(httpResponse.status).to.equal(loginMock.status.BAD_REQUEST)
+    expect(httpResponse.body).to.be.deep.equal(loginMock.errorMessage.REQUIRED_FIELD)
+  })
+  it('should return 401 when username is invalid', async function () {
+    //arrange
+    const httpBody = loginMock.invalidUserNameLogin
+    sinon.stub(UserModel, 'findOne').resolves(null)
+    //act
+    const httpResponse = await chai.request(app).post('/login').send(httpBody)
+    //assert
+    expect(httpResponse.status).to.equal(loginMock.status.UNAUTHORIZED)
+    expect(httpResponse.body).to.be.deep.equal(loginMock.errorMessage.INVALID_LOGIN)
+  })
+  it('should return 401 when password is invalid', async function () {
+    //arrange
+    const httpBody = loginMock.invalidPasswordLogin
+    const mockResponse = UserModel.build(loginMock.loginHagar)
+    sinon.stub(UserModel, 'findOne').resolves(mockResponse)
+    //act
+    const httpResponse = await chai.request(app).post('/login').send(httpBody)
+    //assert
+    expect(httpResponse.status).to.equal(loginMock.status.UNAUTHORIZED)
+    expect(httpResponse.body).to.be.deep.equal(loginMock.errorMessage.INVALID_LOGIN)
+  })
+  it('should return 200 when login is successful', async function () {
+    //arrange
+    const httpBody = loginMock.validLogin
+    const mockResponse = UserModel.build(loginMock.loginHagar)
+    sinon.stub(UserModel, 'findOne').resolves(mockResponse)
+    //act
+    const httpResponse = await chai.request(app).post('/login').send(httpBody)
+    //assert
+    expect(httpResponse.status).to.equal(loginMock.status.OK)
+    expect(httpResponse.body).to.have.property('token')
+  })
 });
---
+++ tests/mocks/login.mock.ts
@@ -0,0 +1,39 @@
+import bcrypt from 'bcryptjs';
+
+const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
+const loginHagar = {
+  username: 'Hagar',
+  vocation: 'Guerreiro',
+  level: 10,
+  password: bcrypt.hashSync('terrível', SALT_ROUNDS),
+}
+const validUserName = 'Hagar'
+const validPassword = bcrypt.hashSync('terrível', SALT_ROUNDS)
+const noUserLogin = { username: '', password: validPassword }
+const noPasswordLogin = { username: validUserName, password: '' }
+const invalidUserNameLogin = { username: 'Bode', password: validPassword }
+const invalidPasswordLogin = { username: validUserName, password: 'bode' }
+const validLogin = { username: validUserName, password: validPassword }
+const fakeToken = { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJIYWdhciIsImlhdCI6MTY4Njc1NDc1Nn0.jqAuJkcLp0RuvrOd4xKxtj_lm3Z3-73gQQ9IVmwE5gA" }
+const status = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+}
+const errorMessage = {
+  REQUIRED_FIELD: { "message": "\"username\" and \"password\" are required" },
+  INVALID_LOGIN: { "message": "Username or password invalid" }
+}
+export default {
+  validPassword,
+  validUserName,
+  noUserLogin,
+  noPasswordLogin,
+  invalidUserNameLogin,
+  invalidPasswordLogin,
+  validLogin,
+  fakeToken,
+  status,
+  errorMessage,
+  loginHagar
+}

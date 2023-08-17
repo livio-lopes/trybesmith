527ffd3f5f18f9864c955bfb3815b72e09a32ad4
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-16 22:32:20 -0300

    refactor(token): add types on functions of token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,6 +1,7 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { User } from '../types/User';
 
 const secret: string = process.env.JWT_SECRET || 'secret';
-const sign = (payload: any):string => jwt.sign(payload, secret, { algorithm: 'HS256' });
-const verify = (token: string):any => jwt.verify(token, secret);
+const sign = (payload: User):string => jwt.sign(payload, secret, { algorithm: 'HS256' });
+const verify = (token: string):JwtPayload | string => jwt.verify(token, secret);
 export default { sign, verify };

6b276deeecf130d69cd834f043cc2aa2372ab801
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-22 10:03:52 -0300

    test(unit): test unit of ordersService

--- tests/unit/services/orders.service.test.ts
+++ tests/unit/services/orders.service.test.ts
@@ -3,7 +3,7 @@ import sinon from 'sinon';
 import ordersMock from '../../mocks/orders.mock';
 const OrdersService = require('../../../src/services/orders.service').default;
 const OrderModel = require('../../../src/database/models/order.model').default;
-
+const ProductModel = require('../../../src/database/models/product.model').default;
 describe('OrdersService', function () {
   beforeEach(function () { sinon.restore(); });
   it('should be orders are listed', async function () {
@@ -14,5 +14,15 @@ describe('OrdersService', function () {
     const mockResponse = ordersMock.responseService;    
     //Assert
     expect(serviceResponse).to.be.deep.equal(mockResponse)
+  })
+  it('should be orders are created', async function () {
+    // Arrange
+    const orderResponse = OrderModel.build(ordersMock.responseServiceCreate);
+    sinon.stub(OrderModel, 'create').resolves(orderResponse);
+    sinon.stub(ProductModel, 'update').resolves();
+    //Act
+    const serviceResponse = await OrdersService.createOrder(ordersMock.createOrderValid);
+    //Assert
+    expect(serviceResponse).to.be.deep.equal(ordersMock.responseServiceCreate);
   })
 });

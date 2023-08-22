7ddd490ef97ce588f18f048ea124d7951b0a6348
Lívio Lopes <antonio.lvio760@gmail.com>
2023-08-22 09:54:06 -0300

    test(mocks): add mocks of order tests integration and unit

--- tests/mocks/orders.mock.ts
+++ tests/mocks/orders.mock.ts
@@ -1,30 +1,44 @@
+import bcrypt from 'bcryptjs';
+
+const SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS || 10;
+
+const user = {
+  username: 'Hagar',
+  vocation: 'Guerreiro',
+  level: 10,
+  password: bcrypt.hashSync('terrível', SALT_ROUNDS),
+}
 const listOrders = [
-  {dataValues: 
   {
-    "id": 1,
-    "userId": 1,
-    "productIds": [
-      {
-        "id": 1
-      },
-      {
-        "id": 2
-      }
-    ]
-  }},
-  {dataValues:
+    dataValues:
+    {
+      "id": 1,
+      "userId": 1,
+      "productIds": [
+        {
+          "id": 1
+        },
+        {
+          "id": 2
+        }
+      ]
+    }
+  },
   {
-    "id": 2,
-    "userId": 3,
-    "productIds": [
-      {
-        "id": 3
-      },
-      {
-        "id": 4
-      }
-    ]
-  }},
+    dataValues:
+    {
+      "id": 2,
+      "userId": 3,
+      "productIds": [
+        {
+          "id": 3
+        },
+        {
+          "id": 4
+        }
+      ]
+    }
+  },
 ]
 
 const responseService = [
@@ -46,6 +60,85 @@ const responseService = [
   },
 ]
 
-const statusCode = { OK:200}
+const createOrderValid = {
+  "productIds": [1, 2],
+  "userId": 1
+}
 
-export default { listOrders, statusCode,responseService }
+const responseServiceCreate = {
+  "id": 4,
+  "userId": 1,
+}
+
+const invalidProductIdsType = {
+  "productIds": { invalid: 'invalid' },
+  "userId": 1
+}
+
+const invalidProductIdsEmpty ={
+  "productIds": [],
+  "userId": 1
+}
+
+const invalidProductIdsTypeArray = {
+  "productIds": ['invalid'],
+  "userId": 1
+}
+
+const noProductsIds = {
+  "productIds": undefined,
+  "userId": 1
+}
+
+const noUserId = {
+  "productIds": [1, 2],
+  "userId": undefined
+}
+const invalidUserIdType = {
+  "productIds": [1, 2],
+  "userId": 'invalid'
+}
+
+const invalidUserIdNotFound = {
+  "productIds": [1, 2],
+  "userId": 545
+}
+
+const validToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJIYWdhciIsInZvY2F0aW9uIjoiR3VlcnJlaXJvIiwibGV2ZWwiOjEwLCJwYXNzd29yZCI6IiQyYSQxMCQ3MDF5Y0Vha00yY2FhMHFVd3MvQ3dPQ0kyTS5rUHppaHBsRFJwSy9vOWxVUmVjUVdPSU0uSyIsImlhdCI6MTY5MjYyNTkyNn0.iMXb-7cfGjIY0WKyRvwUZNfXlxMSVTMyk8rPS6Gx0dU'
+
+const statusCode = { OK: 200, 
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  UNPROCESSABLE_ENTITY: 422,
+ }
+
+ const messageErro ={
+  TOKEN_REQUIRED: { "message": "Token not found" },
+  TOKEN_INVALID: { "message": "Invalid token" },
+  USER_REQUIRED: { "message": "\"userId\" is required" },
+  USER_INVALID: { "message": "\"userId\" must be a number" },
+  USER_NOT_FOUND: { "message": "\"userId\" not found" },
+  PRODUCT_REQUIRED: { "message": "\"productIds\" is required" },
+  PRODUCT_INVALID: { "message": "\"productIds\" must be an array" },
+  PRODUCT_INVALID_TYPE: { "message": "\"productIds\" must include only numbers" },
+ }
+
+export default {
+  listOrders,
+  statusCode,
+  responseService,
+  createOrderValid,
+  invalidProductIdsType,
+  invalidProductIdsTypeArray,
+  noProductsIds,
+  noUserId,
+  invalidUserIdType,
+  invalidUserIdNotFound,
+  messageErro,
+  responseServiceCreate,
+  user,
+  validToken,
+  invalidProductIdsEmpty
+}
